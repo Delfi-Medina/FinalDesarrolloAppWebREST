6a6337828a987d243d43413338ed4690fdc1f3b5
Delfi-medina <delfimedina02@gmail.com>
2024-07-16 13:45:11 -0300

    API terminada

---
+++ Gastos.API/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Gastos.API.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gastos.API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        { 
+        }
+        
+        public DbSet<Gasto> Gastos { get; set; }
+    }
+}
---
+++ Gastos.API/Modelo/Gasto.cs
@@ -0,0 +1,10 @@
+﻿namespace Gastos.API.Modelo
+{
+    public class Gasto
+    {
+        public int Id { get; set; }
+        public string? Descripcion { get; set; }
+        public int Monto { get; set; }
+        public DateTime Fecha { get; set; }
+    }
+}
