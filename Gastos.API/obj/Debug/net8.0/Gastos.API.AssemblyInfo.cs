36526d4a8ceaddf64e577cece2e95784a9325a69
Delfi-medina <delfimedina02@gmail.com>
2024-07-19 04:18:29 -0300

    Correccion de Errores 3

--- Gastos.API/obj/Debug/net8.0/Gastos.API.AssemblyInfo.cs
+++ Gastos.API/obj/Debug/net8.0/Gastos.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gastos.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ea99cb9c1754a3a758a70c00303d5c3fc9a1bfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f4b60835a12fc9aa3c013e265f79124cec6f591")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gastos.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gastos.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
